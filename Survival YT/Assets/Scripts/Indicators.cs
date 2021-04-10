df9049364d115736f75b32c657c5f9721d8ee8f4
maximbsb <simplergamedev@gmail.com>
2021-04-10 21:21:10 +0200

    Character indicators
    Добавил индикаторы персонажа - здоровье, голод, жажда

---
+++ Survival YT/Assets/Scripts/Indicators.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Indicators : MonoBehaviour
+{
+    public Image healthBar, foodBar, waterBar;
+    public float healthAmount = 100;
+    public float foodAmount = 100;
+    public float waterAmount = 100;
+
+    public float secondsToEmptyFood = 60f;
+    public float secondsToEmptyWater = 30f;
+    public float secondsToEmptyHealth = 60f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.fillAmount = healthAmount / 100;
+        foodBar.fillAmount = foodAmount / 100;
+        waterBar.fillAmount = waterAmount / 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (foodAmount > 0)
+        {
+            foodAmount -= 100 / secondsToEmptyFood * Time.deltaTime;
+            foodBar.fillAmount = foodAmount / 100;
+        }
+        if (waterAmount > 0)
+        {
+            waterAmount -= 100 / secondsToEmptyWater * Time.deltaTime;
+            waterBar.fillAmount = waterAmount / 100;
+        }
+
+        if(foodAmount <= 0)
+        {
+            healthAmount -= 100 / secondsToEmptyHealth * Time.deltaTime;
+        }
+        if(waterAmount <= 0)
+        {
+            healthAmount -= 100 / secondsToEmptyHealth * Time.deltaTime;
+        }
+        healthBar.fillAmount = healthAmount / 100;
+
+    }
+}
